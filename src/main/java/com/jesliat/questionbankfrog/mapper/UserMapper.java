493aaef8f0fae62e27ae3c2c4f28c7751ca9cb32
Jesliat Johnson <jesliatjohnson@JesliatdeMacBook-Pro.local>
2024-10-22 19:37:56 +0800

    Initial commit

---
+++ src/main/java/com/jesliat/questionbankfrog/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.jesliat.questionbankfrog.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jesliat.questionbankfrog.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *   
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
